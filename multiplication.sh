0309ff6442b307d3ead7899c3af160c67c09ccdf
FATHIMAJUBINPK <fathimajubin2001@gmail.com>
2025-04-15 11:06:32 +0530

    Update multiplication.sh

--- multiplication.sh
+++ multiplication.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-read -p "Enter a number to print its multiplication table: " num
+echo "Enter a number to print its multiplication table: "
+read num
 i=1
 while [ $i -le 10 ]; do
     echo "$num * $i = $((num * i))"
