ba5084cae900f8d01691970a01554ca6d98a7996
FATHIMA JUBIN PK <fathimajubin2001@gmail.com>
2025-05-22 15:13:17 +0530

    Update Q4.sh

--- rcord/Q4.sh
+++ rcord/Q4.sh
@@ -1,27 +1,29 @@
-Q)Write a shell script program to check the largest among three numbers.
+Q)Write a shell script program to test whether given year is a leap year.  
 
 #!/bin/bash
 clear
-read -p "Enter 3 numbers : " a b c
-if [[ $a -gt $b  && $a -gt $c ]]
+read -p "Enter the year : " l
+if (( $l % 400 == 0 ))
 then
-	echo "$a is the Largest"
-elif [[ $b -gt $c ]]
+	echo "$l is a leapyear "
+elif (( $l % 4 == 0 && $l % 100 !=0 ))
 then
-	echo "$b is the Largest"
+	echo "$l is a leapyear"
 else
-	echo "$c is the largest"
+	echo "$l is not a leapyear"
 fi
 
-
 Output:
-Enter 3 numbers : 5 10 15
-15 is the largest
+Enter the year : 2000
+2000 is a leapyear
+
+
 
 Algorithm:
-Step 1: Ask the user to enter three numbers.
-Step 2: Store the inputs in variables a, b, and c.
-Step 3: Compare a with b and c to check if a is the largest.
-Step 4: If a is the largest, print that a is the largest.
-Step 5: If b is larger than c, print that b is the largest.\
-Step 6: If neither of the above conditions is true, print that c is the largest.
+1. Step 1: Ask the user to enter a year.
+2. Step 2: Store the input in variable `l`.
+3. Step 3: Check if the year is divisible by 400.
+4. Step 4: If true, print that the year is a leap year.
+5. Step 5: If false, check if the year is divisible by 4 but not by 100.
+6. Step 6: If true, print that the year is a leap year.
+7. Step 7: If none of the above conditions are true, print that the year is not a leap year.
